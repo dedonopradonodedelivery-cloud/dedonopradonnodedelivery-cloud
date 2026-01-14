88bb574b8758435edd8a7bb4b8c38e0fde7341c0
dedonopradonodedelivery-cloud <dedonopradonodedelivery@gmail.com>
2026-01-14 15:07:06 -0300

    feat: Simplify MasterSponsorBanner usage
    Removes explicit type prop from MasterSponsorBanner, allowing it to infer type from context. This simplifies usage and reduces potential for errors.

--- components/StoreAdsModule.tsx
+++ components/StoreAdsModule.tsx
@@ -286,7 +286,7 @@ export const StoreAdsModule: React.FC<StoreAdsModuleProps> = ({ onBack }) => {
         </div>
 
         {/* MASTER SPONSOR BANNER (INSTITUTIONAL) */}
-        <MasterSponsorBanner type="merchant" />
+        <MasterSponsorBanner />
 
       </div>
     </div>
